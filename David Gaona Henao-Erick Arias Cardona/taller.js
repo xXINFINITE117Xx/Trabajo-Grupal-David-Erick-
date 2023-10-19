4442e3f3c6fd78977119553151373703f1e57e99
ErickAriasC <eariascardona0@soy.sena.edu.co>
2023-10-19 11:10:44 -0500

    se realiza el ejercicio 7 y se mueven los archivos correcpondientes al taller a la carpeta David Gaona Henao-Erick Arias Cardona/

--- David Gaona Henao-Erick Arias Cardona/taller.js
+++ David Gaona Henao-Erick Arias Cardona/taller.js
@@ -1,4 +1,6 @@
+//message
 let msg = "A continuación le estoy entregando una cadena de texto que servirá como texto guía para realizar los siguientes ejercicios . "
+
 document.addEventListener('DOMContentLoaded', function() {
     const commentTextarea = document.getElementById('comment');
     const counterElement = document.getElementById('counter');
@@ -33,17 +35,35 @@ submitCommentButton.addEventListener('click', function() {
     console.log(commentTextModified);
   });
 });
+
 // 6. Realiza en el DOM un software que compare si dos campos de password son iguales.
+      //EL EJERCICIO SE ENCUENTRA EN LA CAPETA EJERCICIO 6
 
 // 7. Crea una función que valide cuántas vocales tiene el msg entregado.
 
-// 8. Realiza un programa basado en el mockup entregado, que guarde los comentarios agregados por el usuario, teniendo en cuenta lo siguiente:
+const msgCollection = msg.split('')//se convierte en un array el mensaje 
+let counter=0;//contador de vocales
 
+function validateVocals() {
+  //se recorre el arraya para encontrar vocales
+  for (const elemento of msgCollection) {
+    //condicion para validar si hay vocales y aumentar el contador
+    if (elemento=='a'|| elemento=='e'||elemento=='i'||elemento=='o'||elemento=='u' || elemento=='á'|| elemento=='é'||elemento=='í'||elemento=='ó'||elemento=='u') {
+      counter++;
+    } 
+  }
+  return counter
+}
+validateVocals();
+console.log(`El mensaje tiene: ${counter} Vocales`);
+
+// 8. Realiza un programa basado en el mockup entregado, que guarde los comentarios agregados por el usuario, teniendo en cuenta lo siguiente:
+    
 // * No puede haber espacios ni al principio ni al final.
 // * Debe poder convertirlos a mayúscula o a minúscula.
 // * debe tener un contador que va disminuyendo conforme van terminándose el límite de caracteres que este será de 255.
 // * Los comentarios deberán salir en color verde y negrilla.
-
+    //ESTE EJERCICIO SE ENCUENTRA EN EL JS programa.js
 
 // Fecha de entrega(por parejas) --> Jueves 19 de octubre de 2023 a las 11.30 pm -> Link del repositorio.
 // Nombre completo de los integrantes (David Gaona Henao-Erick Arias Cardona)
